0c0c9648be74ab8b3fca0f8f7a7153ce507e70b3
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2019-09-15 07:49:50 +0530

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import sys
+
+filename=sys.argv[1] # we pass filename as command-line-argument here i.e. python3 car.py vehicle.jpg
+car_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+img=cv.imread(filename)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # convert image to gray scale
+faces = car_cascade.detectMultiScale(gray, 1.1, 1) # apply haar-cascade
+for (x,y,w,h) in faces:                            ## for each car found in the image
+  cv.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)  ## draw a rectangle (in RED COLOR of WIDTH 2) around the car
+cv.imwrite("detected_faces.jpg",img)
